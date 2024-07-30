096015d0260e0080d93459571f113f921a02820d
Aniket Desai <aniketd936@gmail.com>
2024-07-30 21:23:42 +0530

    Oauth2 mapping changes

--- airbnb-frontend/src/environments/environment.ts
+++ airbnb-frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  API_URL:"//https://airbnbclonefrontend.onrender.com/api"
+  production: true,
+  API_URL:"http://airbnbclone-production-6a05.up.railway.app/api"
+  // "//airbnbclonefrontend.onrender.com/api"
   //  "//airbnbclone-production-6a05.up.railway.app/api"
 };
