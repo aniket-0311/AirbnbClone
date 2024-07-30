12401e6f3f39d655d56e6d556cda98b85cab9d3e
Aniket Desai <aniketd936@gmail.com>
2024-07-30 21:28:20 +0530

    frontend url added

--- airbnb-frontend/src/environments/environment.development.ts
+++ airbnb-frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  API_URL:"http://airbnbclone-production-6a05.up.railway.app/api"
+  API_URL:"https://airbnbclonefrontend.onrender.com/api"
   // API_URL:"//localhost:4200/api"
 };
--- airbnb-frontend/src/environments/environment.ts
+++ airbnb-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL:"http://airbnbclone-production-6a05.up.railway.app/api"
+  API_URL:"https://airbnbclonefrontend.onrender.com/api"
   // "//airbnbclonefrontend.onrender.com/api"
   //  "//airbnbclone-production-6a05.up.railway.app/api"
 };
