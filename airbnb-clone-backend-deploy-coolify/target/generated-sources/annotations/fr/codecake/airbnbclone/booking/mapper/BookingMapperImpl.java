2a0d05d6b8f15ae741399c417d646e050a1d056a
Aniket Desai <aniketd936@gmail.com>
2024-09-20 12:59:11 +0530

    deployment changes added-4

---
+++ airbnb-clone-backend-deploy-coolify/target/generated-sources/annotations/fr/codecake/airbnbclone/booking/mapper/BookingMapperImpl.java
@@ -0,0 +1,48 @@
+package fr.codecake.airbnbclone.booking.mapper;
+
+import fr.codecake.airbnbclone.booking.application.dto.BookedDateDTO;
+import fr.codecake.airbnbclone.booking.application.dto.NewBookingDTO;
+import fr.codecake.airbnbclone.booking.domain.Booking;
+import java.time.OffsetDateTime;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-09-20T12:52:15+0530",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
+)
+@Component
+public class BookingMapperImpl implements BookingMapper {
+
+    @Override
+    public Booking newBookingToBooking(NewBookingDTO newBookingDTO) {
+        if ( newBookingDTO == null ) {
+            return null;
+        }
+
+        Booking booking = new Booking();
+
+        booking.setStartDate( newBookingDTO.startDate() );
+        booking.setEndDate( newBookingDTO.endDate() );
+
+        return booking;
+    }
+
+    @Override
+    public BookedDateDTO bookingToCheckAvailability(Booking booking) {
+        if ( booking == null ) {
+            return null;
+        }
+
+        OffsetDateTime startDate = null;
+        OffsetDateTime endDate = null;
+
+        startDate = booking.getStartDate();
+        endDate = booking.getEndDate();
+
+        BookedDateDTO bookedDateDTO = new BookedDateDTO( startDate, endDate );
+
+        return bookedDateDTO;
+    }
+}
